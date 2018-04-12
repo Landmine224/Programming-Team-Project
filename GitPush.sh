450e8da67a6860f86cc61f1f18829ea342f9c020
Landon Tolman <landmine224@gmail.com>
2018-04-12 13:54:49 -0600

    test

---
+++ GitPush.sh
@@ -0,0 +1,15 @@
+#!/bin/bash/
+	cd ~/Documents/Visual\ Studio\ 2017/Projects/Team_Project_Maze/
+	read -p "Do you need to add? (y or n): " add
+	read -p "Do you need to commit? (y or n): " commitd
+	if [[ "$add" == *y* ]]; then
+	read -p "Add file: " file
+	git add $file
+	fi
+	if [[ "$commitd" == *y* ]]; then
+	read -p "Commit: " commit
+	git commit -m $commit
+	fi
+	git push
+	read -n 1 -p "Press any key to continue"
+	
